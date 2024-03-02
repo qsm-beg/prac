6c25b2dfd3f7671b1f9af930351d75ad292d4850
qsm_beg <qwbsya001@myuct.ac.za>
2024-03-03 01:28:52 +0200

    added files to front-end

--- src/test.js
+++
@@ -1,9 +0,0 @@
-// const moment = required("moment");
-// const _ = required("lodash");
-// const axios = required("axios").default;
-
-console.log("hello world!!!"); 
-
-// document.write("Ndaa!");
-
-alert("Hi");
